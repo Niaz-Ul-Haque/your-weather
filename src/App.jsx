b1d0443eb884dc38dbd90b48b0cfa0bfeb1eeb2c
Niaz Ul Haque <theniaz619.2@gmail.com>
2020-05-17 07:14:04 -0400

    Temporarily pausing this project, will continue as soon as I get the API key for the Api I wanted to use for this project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import './sass/app.scss';
-import TopSection from './components/top';
+
+import TopSection from './components/top                                                          ';
 import BottomSection from './components/bottom';
 
-function App() {
-  return (
-    <div className="app-container">
-      <div className="main-container">
-        <div className="top-section">
-          <TopSection />
-        </div>
-        <div className="bottom-section">
-          <BottomSection />
+import axios from 'axios';
+
+const WEATHER_KEY = "5f31142f41cf43c7531b509c9f512529";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityName: "Toronto",
+      forcastDays: 5,
+    }
+  }
+
+  componentDidMount() {
+    const {cityName, forcastDayes} = this.state;
+    URL = "http://api.weatherstack.com/current";
+
+    //Atm working here, will continue later on (If I get the API I wanted for this project)
+  }
+  
+  render() {
+    return (
+      <div className="app-container">
+        <div className="main-container">
+          <div className="top-section">
+            <TopSection />
+          </div>
+          <div className="bottom-section">
+            <BottomSection />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App;
